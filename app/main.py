2d8fe881f468cc82c2bfac27195535f1f398e7c9
Vincent112704 <112465584+Vincent112704@users.noreply.github.com>
2026-07-21 20:42:02 +0800

    saving wip

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from app.api.dashboard import router as dashboard_router
 from app.api.table import router as table_router
 from app.api.upload import router as upload_router
 from app.repository.db import supabase
+from app.dependencies.auth import get_user
 
 app = FastAPI(title="FastAPI Boilerplate", version="0.1.0")
+
+
 
 
 @app.get("/")
@@ -29,3 +32,10 @@ def db_check():
 app.include_router(dashboard_router, prefix='/api/dashboard')
 app.include_router(table_router, prefix='/api/table')
 app.include_router(upload_router, prefix='/api/upload')
+
+#TODO:
+#  - Add API logic
+#  - Add Service Logic
+#  - Add repo logic
+#  - finalize auth.user -> public.personnel flow (appending display_name and role)
+#  
